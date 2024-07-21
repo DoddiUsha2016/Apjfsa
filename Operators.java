2fcbe09977c8d1d1395ff9d4889a16c02adb855f
Doddi.Usha <doddiusha0105@gmail.com>
2024-07-21 20:03:42 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,35 @@
+package apjfsa;
+
+public class Operators {
+
+	public static void main(String[] args) {
+			int a=30;
+			int b=5;
+
+			int sum=a+b;
+			int difference=a-b;
+			int product=a*b;
+			int quotient=a/b;
+			int remainder=a%b;
+			//Arithmetic operators
+			System.out.println("Arithmetic Operations");
+			System.out.println("Sum:"+sum);
+			System.out.println("Difference:"+difference);
+			System.out.println("Product:"+product);
+			System.out.println("Quotient:"+quotient);
+			System.out.println("Remainder:"+remainder);
+			//Relational Operators
+			System.out.println("Relational Operations:");
+			System.out.println("Is a equal to b="+(a==b));
+			System.out.println("Is a not equal to b="+(a!=b));
+			System.out.println("Is a greater than b="+(a>b));
+			System.out.println("Is a less than b="+(a<b));
+			System.out.println("Is a greater than or equal to b="+(a>=b));
+			System.out.println("Is a less than or equal to b="+(a<=b));
+
+				}
+
+			}
+	
+
+
